d4f2e55e0d2b124b7465718bb8dcb1d5a27719af
inclavus <inclavus@gmail,com>
2023-02-05 23:39:42 +0530

    Updates

---
+++ precache-manifest.1a962195ea7cc76777a339f12e34e81b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ade40cded7c2be8ce6a1e03bd256489",
+    "url": "/keeper/index.html"
+  },
+  {
+    "revision": "b1b29cf9efb7207dbe7d",
+    "url": "/keeper/static/js/2.df0d89d3.chunk.js"
+  },
+  {
+    "revision": "d158b25544e06c246689",
+    "url": "/keeper/static/js/main.a58e5550.chunk.js"
+  },
+  {
+    "revision": "21676b418b45a5b12e72",
+    "url": "/keeper/static/js/runtime-main.6824ccd7.js"
+  }
+]);
